417eaf0f2cb29626019b4334f7650d768a6f1ed9
Christopher Caldwell <c.caldwell90@gmail.com>
2022-04-21 15:55:11 -0400

    initial checkin

---
+++ examples/index.js
@@ -0,0 +1,37 @@
+const map = L.map('map', {
+  crs: L.CRS.EPSG4326,
+}).setView([45, 15], 3);
+
+const osm = L.tileLayer('https://osm-{s}.gs.mil/tiles/default_pc/{z}/{x}/{y}.png', {
+  subdomains: '1234',
+  attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong>',
+});
+osm.addTo(map);
+
+try {
+  L.geoPackageFeatureLayer([], {
+    geoPackageUrl: 'https://ngageoint.github.io/GeoPackage/examples/rivers.gpkg',
+    layerName: 'rivers',
+    style: function(feature) {
+      return {
+        color: '#F00',
+        weight: 2,
+        opacity: 1,
+      };
+    },
+    onEachFeature: function(feature, layer) {
+      let string = '';
+      for (const key in feature.properties) {
+        string +=
+          '<div class="item"><span class="label">' +
+          key +
+          ': </span><span class="value">' +
+          feature.properties[key] +
+          '</span></div>';
+      }
+      layer.bindPopup(string);
+    }
+  }).addTo(map);
+} catch (e) {
+  console.error(e);
+}
